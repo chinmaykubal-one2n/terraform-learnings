4584b3e715517fc35ff4361c4cbf8240dae94d35
chinmaykubal-one2n <chinmay.kubal@one2n.in>
2025-03-04 16:54:37 +0530

    white spaces removed

--- main.tf
+++ main.tf
@@ -110,28 +110,24 @@ resource "aws_security_group" "nginx_sg" {
   name        = "nginx-sg"
   description = "Allow HTTP and SSH"
   vpc_id      = aws_vpc.demo_vpc.id
-
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = {
     "Name" = "nginx-sg"
   }
