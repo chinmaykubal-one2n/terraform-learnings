315e1660783fb71af96d76c85b4419813c5b7984
chinmaykubal-one2n <chinmay.kubal@one2n.in>
2025-03-05 16:51:30 +0530

    resource aws_lb, aws_lb_target_group, aws_lb_listener, aws_autoscaling_attachment added

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-resource "aws_vpc" "demo_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    "Name" = "demo_vpc"
+    "Name" = "vpc"
   }
 }
 
-resource "aws_internet_gateway" "demo_igw" {
-  vpc_id = aws_vpc.demo_vpc.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
   tags = {
-    "Name" = "demo_igw"
+    "Name" = "igw"
   }
 }
 
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.demo_vpc.id
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
@@ -25,7 +25,7 @@ resource "aws_subnet" "public_subnet_1" {
 }
 
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.demo_vpc.id
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
@@ -35,7 +35,7 @@ resource "aws_subnet" "public_subnet_2" {
 }
 
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.demo_vpc.id
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1a"
   tags = {
@@ -44,7 +44,7 @@ resource "aws_subnet" "private_subnet_1" {
 }
 
 resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.demo_vpc.id
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.4.0/24"
   availability_zone = "us-east-1b"
   tags = {
@@ -53,10 +53,10 @@ resource "aws_subnet" "private_subnet_2" {
 }
 
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.demo_vpc.id
+  vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.demo_igw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
     "Name" = "public_rt"
@@ -86,7 +86,7 @@ resource "aws_nat_gateway" "nat_gw" {
 }
 
 resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.demo_vpc.id
+  vpc_id = aws_vpc.vpc.id
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gw.id
@@ -109,7 +109,7 @@ resource "aws_route_table_association" "private_rt_assoc_2" {
 resource "aws_security_group" "nginx_sg" {
   name        = "nginx-sg"
   description = "Allow HTTP and SSH"
-  vpc_id      = aws_vpc.demo_vpc.id
+  vpc_id      = aws_vpc.vpc.id
   ingress {
     from_port   = 80
     to_port     = 80
@@ -161,3 +161,62 @@ resource "aws_autoscaling_group" "nginx_asg" {
     propagate_at_launch = true
   }
 }
+
+resource "aws_lb" "nginx_lb" {
+  name                       = "nginx-lb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.nginx_sg.id]
+  subnets                    = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  enable_deletion_protection = false
+
+  tags = {
+    "Name" = "nginx-lb"
+  }
+}
+
+resource "aws_lb_target_group" "nginx_tg" {
+  name                              = "nginx-tg"
+  vpc_id                            = aws_vpc.vpc.id
+  target_type                       = "instance"
+  port                              = 80
+  protocol                          = "HTTP"
+  protocol_version                  = "HTTP1"
+  deregistration_delay              = 300
+  load_balancing_algorithm_type     = "round_robin"
+  load_balancing_cross_zone_enabled = "use_load_balancer_configuration"
+  slow_start                        = "0"
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    port                = "traffic-port"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+  tags = {
+    "Name" = "nginx-tg"
+  }
+}
+
+resource "aws_lb_listener" "nginx_listener" {
+  load_balancer_arn = aws_lb.nginx_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx_tg.arn
+  }
+  tags = {
+    "Name" = "nginx-listener"
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.nginx_asg.name
+  lb_target_group_arn    = aws_lb_target_group.nginx_tg.arn
+}
+
+# ALWAYS READ note,txt BEFORE STARTING TO WRITE THE CODE
